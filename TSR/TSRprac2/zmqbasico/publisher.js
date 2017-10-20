3552b722dc556ec2abfe51adb1133362c620a995
stronquens <stronquens@gmail.com>
2017-10-20 19:06:05 +0200

    practica 2 tsr

--- TSR/TSRprac2/zmqbasico/publisher.js
+++ TSR/TSRprac2/zmqbasico/publisher.js
@@ -1,20 +1,29 @@
 var zmq = require('zmq')
 var publisher = zmq.socket('pub')
+var aux = require("./auxfunctions1718.js");
 
-publisher.bind('tcp://*:8688', function(err) {
+var args = process.argv.slice(2);
+var port = args[0];
+var nmen = args[1];
+var desc1 = args[2];
+var desc2 = args[3];
+
+publisher.bind('tcp://*:'+port, function(err) {
   if(err)
     console.log(err)
   else
-    console.log("Listening on 8688...")
+    console.log("Listening on "+port+"...")
 })
 
-for (var i=1 ; i<10 ; i++)
+for (var i=1 ; i<nmen ; i++)
     setTimeout(function() {
         console.log('sent');
-        publisher.send("Hello there!")
+        publisher.send(desc1 + aux.randNumber(0,100));
+        publisher.send(desc2 + aux.randNumber(0,100));
     }, 1000 * i)
 
 process.on('SIGINT', function() {
   publisher.close()
   console.log('\nClosed')
 })
+// node publisher 8000 5 marca as
--- TSR/TSRprac2/zmqbasico/subscriber.js
+++ TSR/TSRprac2/zmqbasico/subscriber.js
@@ -1,14 +1,19 @@
 var zmq = require('zmq')
 var subscriber = zmq.socket('sub')
 
+var args = process.argv.slice(2);
+var endpoint = args[0];
+var descriptor = args[1];
+
 subscriber.on("message", function(reply) {
   console.log('Received message: ', reply.toString());
 })
 
-subscriber.connect("tcp://localhost:8688")
-subscriber.subscribe("")
+subscriber.connect(endpoint.toString())//"tcp://localhost:8688"
+subscriber.subscribe(descriptor.toString())
 
 process.on('SIGINT', function() {
   subscriber.close()
   console.log('\nClosed')
 })
+//node subscriber tcp://localhost:8000 marca
